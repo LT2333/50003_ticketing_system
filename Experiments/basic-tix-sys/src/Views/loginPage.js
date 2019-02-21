000ac1492722c1858495e9407d29bd3c3ef08873
JQ <jingqi_chan@mymail.sutd.edu.sg>
2019-02-21 20:28:55 +0800

    added state values for login and signup

--- Experiments/basic-tix-sys/src/Views/loginPage.js
+++ Experiments/basic-tix-sys/src/Views/loginPage.js
@@ -1,34 +1,64 @@
 import React, { Component } from "react";
 import { Form, FormInput, FormGroup, Button, NavLink } from "shards-react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import Homepage from "./homePage";
 
 class LoginCreds extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    console.log(
+      "Login details were submitted: \n" +
+        "username: " +
+        this.state.username +
+        "\npassword: " +
+        this.state.password
+    );
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <Router>
-        <div class="w-50 mx-auto">
-          <h1>Courier Tix</h1>
-          <Form>
-            <FormGroup>
-              <label>Username</label>
-              <FormInput placeholder="Username" />
-            </FormGroup>
+      <div class="w-50 mx-auto">
+        <h1>Courier Tix</h1>
+        <Form>
+          <FormGroup>
+            <label>Username</label>
+            <FormInput
+              placeholder="Username"
+              name="username"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
 
-            <FormGroup>
-              <label>Password</label>
-              <FormInput type="password" placeholder="Password" />
-            </FormGroup>
-          </Form>
-          <Link to={Homepage}>
-            <Button>Login</Button>
-          </Link>
-          <Link to="/signupPage">
-            <NavLink>New user? Register here!</NavLink>
-          </Link>
-        </div>
-      </Router>
+          <FormGroup>
+            <label>Password</label>
+            <FormInput
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+        </Form>
+        <Button onClick={this.handleSubmit}>Login</Button>
+        <NavLink>New user? Register here!</NavLink>
+      </div>
     );
   }
 }
--- Experiments/basic-tix-sys/src/Views/signupPage.js
+++ Experiments/basic-tix-sys/src/Views/signupPage.js
@@ -3,7 +3,59 @@ import { Form, FormInput, FormGroup, Container, Button } from "shards-react";
 import "./signupPage.css";
 
 class SignupCreds extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      secondpass: "",
+      emailValid: false,
+      passValid: false,
+      formValid: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  handlePassword(event) {
+    this.setState({ secondpass: event.target.secondpass });
+
+    if (this.state.secondpass == this.state.password) {
+      this.setState({ valid: true });
+    }
+
+    if (this.state.valid) {
+      console.log("OKAY");
+    } else {
+      console.log("NOT OKAY");
+    }
+  }
+
+  handleSubmit(event) {
+    console.log(
+      "New signup: \n" +
+        "username: " +
+        this.state.username +
+        "\nemail: " +
+        this.state.email +
+        "\npassword: " +
+        this.state.password +
+        "\nre-entered password:" +
+        this.state.secondpass
+    );
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div class="w-50 mx-auto">
@@ -11,23 +63,41 @@ class SignupCreds extends Component {
         <Form>
           <FormGroup>
             <label>Username</label>
-            <FormInput placeholder="Enter a username" />
+            <FormInput
+              placeholder="Enter a username"
+              name="username"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <label>Email Address</label>
-            <FormInput type="email" placeholder="Enter your email" />
+            <FormInput
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <label>Password</label>
-            <FormInput type="password" placeholder="Enter a password" />
+            <FormInput
+              type="password"
+              placeholder="Enter a password"
+              name="password"
+              onChange={this.handleChange}
+            />
             <FormInput
               className="secondpass"
+              name="secondpass"
               type="password"
               placeholder="Re-enter password"
+              onChange={this.handlePassword}
             />
           </FormGroup>
         </Form>
-        <Button type="submit">Register</Button>
+        <Button onClick={this.handleSubmit} disabled={!this.state.valid}>
+          Register
+        </Button>
       </div>
     );
   }
