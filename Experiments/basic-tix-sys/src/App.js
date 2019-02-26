22e313758c9a9a1318fd7f875d6135cdf4411c1d
JQ <jingqi_chan@mymail.sutd.edu.sg>
2019-02-26 20:59:07 +0800

    added routing and header with simple sidebar

--- Experiments/basic-tix-sys/src/App.js
+++ Experiments/basic-tix-sys/src/App.js
@@ -4,18 +4,30 @@ import SignupCreds from "./Views/signupPage";
 import LoginCreds from "./Views/loginPage";
 import Homepage from "./Views/homePage";
 import ContactUs from "./Components/contactUsForm";
+import HeaderBar from "./Components/headerBar";
 import "bootstrap/dist/css/bootstrap.css";
 import "shards-ui/dist/css/shards.min.css";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import SideBar from "./Components/sideBar";
+import { Container } from "shards-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LoginCreds />
-        <SignupCreds />
-        <ContactUs />
-      </div>
+      <Router>
+        <div className="App">
+          <Container>
+            <HeaderBar />
+            {/* <SideBar /> */}
+          </Container>
+
+          <Switch>
+            <Route path="/login" component={LoginCreds} />
+            <Route path="/contactus" component={ContactUs} />
+            <Route path="/signup" component={SignupCreds} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ Experiments/basic-tix-sys/src/Components/sideBar.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+
+class SideBar extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <Route
+          render={({ location, history }) => (
+            <React.Fragment>
+              <SideNav
+                onSelect={selected => {
+                  const to = "/" + selected;
+                  if (location.pathname !== to) {
+                    history.push(to);
+                  }
+                }}
+              >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                  <NavItem eventKey="home">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-home"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Home</NavText>
+                  </NavItem>
+                  <NavItem eventKey="devices">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-device"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Devices</NavText>
+                  </NavItem>
+                </SideNav.Nav>
+              </SideNav>
+            </React.Fragment>
+          )}
+        />
+      </Router>
+    );
+  }
+}
+
+export default SideBar;
