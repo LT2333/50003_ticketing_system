8937908534d05f1b35a0dbd2b6793427465bd93c
JQ <jingqi_chan@mymail.sutd.edu.sg>
2019-03-01 03:03:27 +0800

    added message boards

--- Experiments/basic-tix-sys/src/Components/headerBar.js
+++ Experiments/basic-tix-sys/src/Components/headerBar.js
@@ -37,6 +37,9 @@ class HeaderBar extends Component {
           <NavItem>
             <NavLink href="/contactus">Contact Us</NavLink>
           </NavItem>
+          {/* <NavItem>
+            <NavLink href="/messagepage">Message Box</NavLink>
+          </NavItem> */}
         </Nav>
 
         <Nav navbar className="ml-auto">
