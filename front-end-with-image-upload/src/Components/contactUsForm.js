bb00ff2b2a041bf71c8a1d9722b261e53a7620cf
LT2333 <0817rainy@gmail.com>
2019-04-05 03:05:00 +0800

    no-upload-button-for-image

--- front-end-with-image-upload/src/Components/contactUsForm.js
+++ front-end-with-image-upload/src/Components/contactUsForm.js
@@ -69,7 +69,7 @@ class ContactUs extends Component {
     this.toggle = this.toggle.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeSelect = this.handleChangeSelect.bind(this);
-    //this.singleFileChangedHandler = this.singleFileChangedHandler.bind(this);  //Here
+    this.singleFileChangedHandler = this.singleFileChangedHandler.bind(this);  //Here
   }
 
   handleChange(event) {
@@ -106,7 +106,6 @@ class ContactUs extends Component {
         this.state.relatedtags+
         "\nuploadedImage: "+
         this.state.selectedFile   //Here
-    
     );
 
     //   var unirest = require("unirest");
@@ -132,7 +131,8 @@ class ContactUs extends Component {
 
     //     console.log(res.body);
     //   });
-
+    
+    
     
     var unirest = require("unirest");
 
@@ -155,14 +155,52 @@ class ContactUs extends Component {
       uploadedImage: this.state.selectedFile  //Here
     });
 
+     //******************* */
+     const data = new FormData();
+     // If file selected
+     if ( this.state.selectedFile ) {
+     data.append( 'profileImage', this.state.selectedFile, this.state.selectedFile.name );
+     axios.post( 'http://localhost:5000/api/profile-img-upload', data, {
+       headers: {
+        'accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+       }
+      })
+       .then( ( response ) => {
+     if ( 200 === response.status ) {
+       // If file size is larger than expected.
+       if( response.data.error ) {
+        if ( 'LIMIT_FILE_SIZE' === response.data.error.code ) {
+         this.ocShowAlert( 'Max size: 2MB', 'red' );
+        } else {
+         console.log( response.data );
+     // If not the given file type
+         this.ocShowAlert( response.data.error, 'red' );
+        }
+       } else {
+        // Success
+        let fileName = response.data;
+        console.log( 'fileData', fileName );
+        this.ocShowAlert( 'File Uploaded', '#3089cf' );
+       }
+        }
+       }).catch( ( error ) => {
+       // If another error
+       this.ocShowAlert( error, 'red' );
+      });
+     } else {
+      // if file not selected throw error
+      this.ocShowAlert( 'Please upload file', 'red' );
+     }
+
     req.end(function(res) {
       if (res.error) throw new Error(res.error);
 
       console.log(res.body);
-    
     });
-
     
+   
     
   }
 ///***************************************Starts here*************************************** */
@@ -176,6 +214,10 @@ class ContactUs extends Component {
 
 	//"Real" upload! after the upload button is clicked
 	singleFileUploadHandler = ( event ) => {
+                  //  <div className="mt-5">
+                  //   <button className="btn btn-info" onClick={this.singleFileUploadHandler}>Upload Image</button>
+                  //  </div>
+
 		const data = new FormData();
 	  // If file selected
 		if ( this.state.selectedFile ) {
@@ -303,11 +345,9 @@ class ContactUs extends Component {
                   <div className="card-body">
                     <p className="card-text">Attachment</p>
                     <input type="file" onChange={this.singleFileChangedHandler}/>
-                    <div className="mt-5">
-                      <button className="btn btn-info" onClick={this.singleFileUploadHandler}>Upload Image</button>
-                    </div>
                   </div>
-             
+
+               
                   <Button onClick={this.toggle}>Submit Now!</Button>
                   
                   <Modal open={open} toggle={this.toggle}>
