6cfe68ee0b79c37a5215e3312815201a46f88f2a
LT2333 <0817rainy@gmail.com>
2019-04-05 02:39:33 +0800

    working-upload-image

---
+++ front-end-with-image-upload/work-basic-tix-sys-pm3/src/Components/chatInput.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormInput,
+  FormGroup,
+  Button,
+  NavLink,
+  Container,
+  InputGroup,
+  InputGroupAddon,
+  Row
+} from "shards-react";
+import "../Views/signupPage.css";
+import ChatMessages from "./chatMessages";
+
+class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chatInput: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ chatInput: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // Stop the form from refreshing the page on submit
+    event.preventDefault();
+
+    var unirest = require("unirest");
+
+    var reqpost = unirest(
+      "POST",
+      "https://courier50003.herokuapp.com/portal/chats"
+    );
+
+    reqpost.headers({
+      "cache-control": "no-cache",
+      "content-type": "application/json"
+    });
+
+    reqpost.type("json");
+    reqpost.send({
+      admin_id: "5c94643a471b590004e5fd00",
+      request_id: "5c946495471b590004e5fd01",
+      conversastion: this.state.chatInput
+    });
+
+    reqpost.end(res => {
+      if (res.error) throw new Error(res.error);
+
+      console.log(res.body);
+
+      this.setState({ chatInput: "" });
+      console.log("not here");
+      console.log(this.state.chatInput);
+    });
+
+    // Call the onSend callback with the chatInput message
+    // this.props.onSend(this.state.chatInput);
+
+    // Clear the input box
+  }
+
+  render() {
+    console.log("test");
+
+    return (
+      <div>
+        <InputGroup>
+          <FormInput
+            placeholder={this.state.chatInput}
+            name="chatInput"
+            onChange={this.handleChange}
+          />
+          <InputGroupAddon type="append">
+            <Button theme="secondary" onClick={this.handleSubmit}>
+              Send
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    );
+  }
+}
+
+//app
+//chatinput - input box
+//messages - shows list of messages
+//message - show individual message
+
+export default ChatInput;
