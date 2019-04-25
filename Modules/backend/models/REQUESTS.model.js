58609c669164c96e8d582af6ddd2ca056f15f7b3
Glenn Chia <glennchia7@gmail.com>
2019-04-25 19:45:17 +0800

    update request schena

--- Modules/backend/models/REQUESTS.model.js
+++ Modules/backend/models/REQUESTS.model.js
@@ -13,28 +13,29 @@ let RequestsSchema = new Schema({
     name:{type: String}, // This is for users who don't have accounts
     email: {type: String}, //, required: true},
     contact_num: {type: Number}, //require: true},
-    date: {type: Date, default:Date.now}, // auto-generated
-    title:{type: String},
+    date: {type: Date, default:new Date()}, // auto-generated
+    title:{type:String},
     message:{type: String}, // user-typed
-    //imageURL: {type: String, default: ''}, // retrieved from Amazon
+    imageURL: {type: String, default: ''}, // retrieved from Amazon
     img : { data: Buffer, contentType: String },  // use fs for testing
     priority:{type: Number, default:-1}, // Calculated between 0-1, accepts floats
-    status:{type: String, default:'unaddressed'}, // Unattended, addressing, finished
+    status:{type: String, default:'unaddressed'}, // unaddressed, addressing, finished
     who:{type: String, default:'unaddressed'}, // Admin Username. Find a way to link
+    team:{type: String, default:''}, // Keep track of which team is assigned to handle it
     tags:{type: [String]}, // Array of tags of String data type
     category:{type: String, default:''}, // selected from a drop-down
     chat:{type: [
       {
         name:{type:String},
         message: {type:String},
-        date:{type: Date, default:Date.now}
+        date:{type: Date, default:new Date()}
       }
     ]}, // An array of hashMaps
     dateTaken:{type: Date},
     dateComplete:{ type: Date},
-    solution:{type: String, default:''}
+    solution:{type: String, default:''} // At the end when the admin completes the ticket this is filled up
 });
 
 
 // Export the model
-module.exports = mongoose.model('REQUESTS', RequestsSchema);
+module.exports = mongoose.model('REQUEST2', RequestsSchema);
